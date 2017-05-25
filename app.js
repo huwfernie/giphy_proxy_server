e8862def678c23d41e0137466c7fd94e555ce65f
Huw Fernie <HuwFernie@gmail.com>
2017-05-25 12:38:40 +0100

    http.get to giphy works, not able to re-send the data back as JSON yet

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+
+var options = {
+  host: 'api.giphy.com',
+  path: '/v1/gifs/trending?api_key=dc6zaTOxFJmzC'
+};
 
 app.use((request, response, next) => {
   console.log(request.headers);
@@ -12,9 +18,36 @@ app.use((request, response, next) => {
 });
 
 app.get('/', (request, response) => {
-  response.json({
-    chance: request.chance
-  });
+  // response.json(sendReq());
+  response.json({huw: 'huw'});
+});
+
+
+
+app.use((err, request, response, next) => {
+  // log the error, for now just console.log
+  console.log(err);
+  response.status(500).send('Something broke!');
 });
 
 app.listen(3000);
+
+function  sendReq() {
+  http.get(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+    // Buffer the body entirely for processing as a whole.
+    var bodyChunks = [];
+    res.on('data', function(chunk) {
+      // You can process streamed parts here...
+      bodyChunks.push(chunk);
+    }).on('end', function() {
+      var body = Buffer.concat(bodyChunks);
+      body = JSON.parse(body);
+      //console.log(body);
+      return body;
+      // ...and/or process the entire body here.
+    });
+  });
+}
