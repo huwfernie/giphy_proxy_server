03abf2790cae39a9969cdd55d815843a1727683e
Huw Fernie <HuwFernie@gmail.com>
2017-05-25 14:03:28 +0100

    studying promisses in app2

--- app2.js
+++ app2.js
@@ -3,36 +3,54 @@ const app = express();
 const http = require('http');
 
 var firstMethod = function() {
-  var promise = new Promise(function(resolve){
-    setTimeout(function() {
-      console.log('first method completed');
-      resolve({data: '123'});
-    }, 1000);
+  var promise = new Promise(function(resolve, reject) {
+    const data = {test: '123'};
+    if (data) {
+      console.log(data);
+      resolve(data);
+    } else {
+      reject('no data');
+    }
+
   });
   return promise;
 };
 
 
-var secondMethod = function(someStuff) {
-  var promise = new Promise(function(resolve){
-    setTimeout(function() {
-      console.log('second method completed');
-      resolve({newData: someStuff.data + ' some more data'});
-    }, 1000);
-  });
-  return promise;
-};
+function success(input) {
+  console.log('success ',input);
+}
 
-var thirdMethod = function(someStuff) {
-  var promise = new Promise(function(resolve){
-    setTimeout(function() {
-      console.log('third method completed');
-      resolve({result: someStuff.newData});
-    }, 2000);
-  });
-  return promise;
-};
+function failure(input) {
+  console.log('failure ',input);
+}
 
 firstMethod()
-   .then(secondMethod)
-   .then(thirdMethod);
+  .then(success())
+  .catch(failure());
+
+
+
+
+
+//
+//
+// var secondMethod = function(someStuff) {
+//   var promise = new Promise(function(resolve){
+//     setTimeout(function() {
+//       console.log('second method completed');
+//       resolve({newData: someStuff.data + ' some more data'});
+//     }, 1000);
+//   });
+//   return promise;
+// };
+//
+// var thirdMethod = function(someStuff) {
+//   var promise = new Promise(function(resolve){
+//     setTimeout(function() {
+//       console.log('third method completed');
+//       resolve({result: someStuff.newData});
+//     }, 2000);
+//   });
+//   return promise;
+// };
